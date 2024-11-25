4b62728dd53d31376b97586db324a5d02fbce974
lynaticherafi <148385892+lynaticherafi@users.noreply.github.com>
2024-11-25 14:49:50 +0100

    colored image size Bitmap function

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,22 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
+    int bits_per_pixel = 24;
+    float size_colored = w * h * bits_per_pixel;
 
+    // Convert the size based on the specified unit
+    if (strcmp(unit, "bt") == 0) {
+        return size_colored;
+    } 
+    else if (strcmp(unit, "ko") == 0) {
+        return (size_colored / 8) / 1024;
+    } 
+    else if (strcmp(unit, "mo") == 0) {
+        return ((size_colored / 8) / 1024) / 1024;
+    } 
+    else if (strcmp(unit, "go") == 0) {
+        return (((size_colored / 8) / 1024) / 1024) / 1024;
+    } 
    // YOUR CODE HERE - END
    return 0;
 }
